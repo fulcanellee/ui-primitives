255b3256ece820d486fa00d9c34a0fcad77c8ef1
Mikhail Lukin <mishalykin@mail.ru>
2025-03-27 14:24:46 +0700

    prepare config

--- utils/publish.sh
+++ utils/publish.sh
@@ -4,7 +4,7 @@ rm -rf .publish
 mkdir .publish
 cp -r {icons,styles,animations,package.json,README.md,.npmrc} .publish
 cd .publish
-npm publish
+npm publish --access public
 
 echo "Publishing package to registry..."
 
